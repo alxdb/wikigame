ff43c356adbc1b9048a114c62e6614a42e31947c
Alexander Davidson Bryan <alxdb@pm.me>
2023-07-20 20:21:18 +0100

    replaced wikigame with aio_wikigame

--- wikigame.py
+++ wikigame.py
@@ -1,55 +1,144 @@
-import wikipediaapi as wapi
-import requests
+import asyncio
+import dataclasses
+import logging
+import urllib.parse
+from typing import AsyncIterator
+
+import aiohttp
 
 
-WAPI = wapi.Wikipedia("WikiGame (alxdb@pm.me)", "en")
+@dataclasses.dataclass(frozen=True, eq=True)
+class Page:
+    title: str
 
 
-def get_random_page() -> wapi.WikipediaPage:
-    response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/title")
-    assert response.status_code == 200
-    title = response.json()["items"][0]["title"]
-    page = WAPI.page(title)
-    assert page.exists()
-    return page
+@dataclasses.dataclass(frozen=True, eq=True)
+class Path:
+    path: list[Page]
 
 
-def found_page() -> None:
-    print("Found link!")
+class WApi:
+    def __init__(self, session: aiohttp.ClientSession):
+        self._session = session
+
+    async def _request(self, params: dict[str, str | int]) -> dict:
+        params.setdefault("action", "query")
+        params.setdefault("format", "json")
+        params.setdefault("formatversion", "2")
+        async with self._session.get(
+            "https://en.wikipedia.org/w/api.php?" + urllib.parse.urlencode(params),
+            headers={"User-Agent": "WikiGame (alxdb@pm.me)"},
+        ) as response:
+            logging.debug(response)
+            return await response.json()
+
+    async def request(self, params: dict) -> AsyncIterator[dict]:
+        response = await self._request(params)
+        logging.debug(response)
+
+        if "error" in response:
+            raise RuntimeError(f"API Error: {response['error']}")
+
+        if "warnings" in response:
+            logging.warn(f"API Warnings: {response['warnings']}")
+
+        while True:
+            yield response["query"]
+
+            if "batchcomplete" in response:
+                break
+
+            params.update(response["continue"])
+            response = await self._request(params)
+
+    async def random_page(self, limit: int = 1) -> AsyncIterator[Page]:
+        async for response in self.request(
+            {
+                "list": "random",
+                "rnnamespace": 0,
+                "rnlimit": limit,
+            }
+        ):
+            for page in response["random"]:
+                yield Page(page["title"])
+
+    async def lookup_page(self, title: str) -> Page:
+        result = await anext(
+            self.request(
+                {
+                    "titles": title,
+                }
+            )
+        )
+        return Page(result["pages"][0]["title"])
+
+    async def page_links(self, page: Page) -> AsyncIterator[Page]:
+        async for response in self.request(
+            {
+                "prop": "links",
+                "titles": page.title,
+                "plnamespace": 0,
+                "pllimit": "max",
+            }
+        ):
+            result = response["pages"][0].get("links")
+            if result is not None:
+                for link in result:
+                    yield Page(link["title"])
+
+    async def links_here(self, page: Page) -> AsyncIterator[Page]:
+        async for response in self.request(
+            {
+                "prop": "linkshere",
+                "titles": page.title,
+                "lhnamespace": 0,
+                "lhlimit": "max",
+            }
+        ):
+            result = response["pages"][0].get("linkshere")
+            if result is not None:
+                for link in result:
+                    yield Page(link["title"])
 
 
-def main() -> None:
-    # src = WAPI.page("Python_(programming_language)")
-    src = get_random_page()
-    assert src.exists()
-    # dst = WAPI.page("MiNT")
-    dst = get_random_page()
-    assert dst.exists()
-    print(f"{src.title} -> {dst.title}")
+async def main():
+    async with aiohttp.ClientSession() as session:
+        wapi = WApi(session)
+        src, tgt = [page async for page in wapi.random_page(2)]
+        print(f"{src=} -> {tgt=}")
 
-    if dst.title in src.links:
-        found_page()
-        return
+        targets = {tgt}
+        target_queue = [tgt]
+        sources = {src}
+        source_queue = [src]
 
-    # Breadth first search
-    to_search: list[list[wapi.WikipediaPage]] = list()
-    searched: set[wapi.WikipediaPage] = set()
+        found_link = False
+        while not found_link:
+            current_source = source_queue.pop(0)
+            source_links = [i async for i in wapi.page_links(current_source)]
+            sources |= set(source_links)
+            print(f"{len(sources)=} {len(targets)=}")
 
-    to_search.append([src])
-    found_dst = False
-    while not found_dst:
-        path = to_search.pop(0)
-        page = path[-1]
-        print(f"Searching: {[page.title for page in path]}")
-        if dst.title in page.links:
-            found_dst = True
-        else:
-            print(f"Not found in: {page.title}")
-            for link in filter(lambda link: link not in searched, page.links.values()):
-                to_search.append(path + [link])
+            if sources & targets:
+                found_link = True
+                print("Found link")
+                break
+            else:
+                source_queue.extend(source_links)
 
-    found_page()
+            current_target = target_queue.pop(0)
+            target_links = [i async for i in wapi.links_here(current_target)]
+            targets |= set(target_links)
+            print(f"{len(sources)=} {len(targets)=}")
+
+            if sources & targets:
+                found_link = True
+                print("Found link")
+                break
+            else:
+                target_queue.extend(target_links)
 
 
 if __name__ == "__main__":
-    main()
+    logging.getLogger().setLevel(logging.INFO)
+    asyncio.run(main())
