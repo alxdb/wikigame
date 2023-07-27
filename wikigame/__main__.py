b5a0b5067a34a5bb89c4ebfd9bd1883c2b59d0bb
Alexander Davidson Bryan <alxdb@pm.me>
2023-07-27 14:06:25 +0100

    remove duplicates in search queue

--- wikigame/__main__.py
+++ wikigame/__main__.py
@@ -9,7 +9,7 @@ async def main():
     async with wikigame.wikiapi.WikiApi() as wikiapi:
         source, target = [page async for page in wikiapi.random_pages(2)]
         print(f"Searching for {target.title} from {source.title}")
-        links = await wikigame.search.find_link(wikiapi, source, target)
+        links = await wikigame.search.find_route(wikiapi, source, target)
         for link in links:
             print(f"{link.title} links pages")
 
--- wikigame/search.py
+++ wikigame/search.py
@@ -1,5 +1,7 @@
 import logging
-from typing import Literal
+from typing import Iterator, Literal, Self
+
+from ordered_set import OrderedSet
 
 from wikigame.wikiapi import Page, WikiApi
 
@@ -8,34 +10,50 @@ SearchMode = Literal["source"] | Literal["target"]
 
 class Search:
     def __init__(self, page: Page, search_mode: SearchMode):
-        self.search_space = {page}
-        self.search_queue = [page]
+        self.search_space: dict[Page, list[Page]] = {page: []}
+        self.search_queue = OrderedSet([page])
         self.search_mode = search_mode
 
-    async def extend_search(self, wikiapi: WikiApi, other: "Search") -> set[Page]:
-        links = await self._find_links(wikiapi)
-        self.search_space |= links
-        result = self.search_space & other.search_space
+    async def extend_search(self, wikiapi: WikiApi, other: Self) -> set[Page]:
+        search_head = self.search_queue.pop(0)
+        logging.info(f"Searching {self.search_mode}: {search_head}")
+
+        links = await self._find_links(search_head, wikiapi)
+
+        # self.search_space |= links
+        for link in links:
+            self.search_space.setdefault(link, []).append(search_head)
+
+        result = self.search_space.keys() & other.search_space.keys()
         if not result:
-            self.search_queue.extend(links)
+            # self.search_queue.extend(links)
+            self.search_queue |= OrderedSet(links)
         return result
 
-    async def _find_links(self, wikiapi: WikiApi) -> set[Page]:
-        search_head = self.search_queue.pop(0)
-        logging.info(f"Searching {search_head}")
-
+    async def _find_links(self, page: Page, wikiapi: WikiApi) -> OrderedSet[Page]:
         match self.search_mode:
             case "source":
                 search_method = wikiapi.links
             case "target":
                 search_method = wikiapi.links_here
             case _:
-                raise RuntimeError("Invalid mode")
+                raise RuntimeError(f"Invalid search mode: {self.search_mode}")
+        return OrderedSet([link async for link in search_method(page)])
 
-        return {page async for page in search_method(search_head)}
+    def find_route(self, page: Page) -> Iterator[Page] | None:
+        if page not in self.search_space:
+            return None
+
+        links = self.search_space[page]
+        while len(links) != 0:
+            if len(links) > 1:
+                logging.warn(f"Ignoring links: {links[1:]}")
+            link = links[0]
+            yield link
+            links = self.search_space[link]
 
 
-async def find_link(wikiapi: WikiApi, source: Page, target: Page) -> set[Page]:
+async def find_route(wikiapi: WikiApi, source: Page, target: Page) -> set[Page]:
     sources = Search(source, "source")
     targets = Search(target, "target")
 
